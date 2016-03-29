fe1478eef0853a15a336464a2b551ea4b7791977
Ben Botto <benjamin.botto@gmail.com>
2016-03-28 20:56:38 -0700

    Initial version with simple example.

--- Generator.js
+++ Generator.js
@@ -1,73 +1,80 @@
 'use strict';
 
-var pluralize    = require('pluralize');
-var util         = require('util');
-var infoSchemaDC = require('./infoSchemaDataContext');
-var dbName       = 'bike_shop';
+var util = require('util');
 
-// Get all the tables and columns from the information_schema db.
-var query = infoSchemaDC
-  .from('tables')
-  .innerJoin
-    ({
-      table:  'columns',
-      parent: 'tables',
-      on:
-      {
-        $and:
-        [
-          {$eq: {'tables.TABLE_NAME':'columns.TABLE_NAME'}},
-          {$eq: {'tables.TABLE_SCHEMA':'columns.TABLE_SCHEMA'}}
-        ]
-      }
-    })
-  .where({$eq: {'tables.TABLE_SCHEMA':':schema'}}, {schema: dbName})
-  .select('tables.TABLE_NAME', 'columns.COLUMN_NAME', 'columns.DATA_TYPE', 'columns.IS_NULLABLE', 'columns.CHARACTER_MAXIMUM_LENGTH')
-  .orderBy('tables.TABLE_NAME', 'columns.COLUMN_NAME');
+/**
+ * Initialize the schema generator.
+ * @param infoSchemaDC A DataContext instance with permission to read
+ *        from the INFORMATION_SCHEMA table.
+ */
+function Generator(infoSchemaDC)
+{
+  this._infoSchemaDC = infoSchemaDC;
+}
 
-//console.log(query.toString());
+/**
+ * Generate the schema from the database.
+ * @param dbName The name of the database for which the schema should be
+ *        generated.
+ * @param tableCB A callback function(table) that is called with each
+ *        table.  The function can be used to set the table alias or perform
+ *        other table-level manipulation.
+ * @param columnCB A callback function(column, table) that is called with
+ *        each column.  The function can be used to set a column alias or
+ *        add converters.
+ */
+Generator.prototype.generateSchema = function(dbName, tableCB, columnCB)
+{
+  tableCB  = tableCB  || function() {};
+  columnCB = columnCB || function() {};
 
-query.execute()
-  .then(function(res)
-  {
-    // Build the schema.
-    var database    = {};
-    database.name   = dbName;
-    database.tables = [];
+  // Get all the tables and columns from the information_schema db.
+  var query = this._infoSchemaDC
+    .from('tables')
+    .innerJoin
+      ({
+        table:  'columns',
+        parent: 'tables',
+        on:
+        {
+          $and:
+          [
+            {$eq: {'tables.TABLE_NAME':'columns.TABLE_NAME'}},
+            {$eq: {'tables.TABLE_SCHEMA':'columns.TABLE_SCHEMA'}}
+          ]
+        }
+      })
+    .where({$eq: {'tables.TABLE_SCHEMA':':schema'}}, {schema: dbName})
+    .select('tables.TABLE_NAME', 'columns.COLUMN_NAME', 'columns.DATA_TYPE', 'columns.IS_NULLABLE', 'columns.CHARACTER_MAXIMUM_LENGTH')
+    .orderBy('tables.TABLE_NAME', 'columns.COLUMN_NAME');
 
-    res.tables.forEach(function(table)
+  //console.log(query.toString());
+
+  query.execute()
+    .then(function(res)
     {
-      var tblAliasSing;
+      // Build the schema.
+      var database    = {};
+      database.name   = dbName;
+      database.tables = [];
 
-      // The table alias removes any underscores and uppercases the proceeding
-      // character.  Ex: bike_shop_bikes => bikeShopBikes
-      table.alias = table.name.replace(/_[a-z]/g, (c) => c.substr(1).toUpperCase());
-
-      // Singular version of the table alias.
-      tblAliasSing = pluralize(table.alias, 1);
-
-      table.columns.forEach(function(col)
+      res.tables.forEach(function(table)
       {
-        // The table alias is removed from any columns.  For example, bike_shops.bikeShopID
-        // becomes bikeShops.ID.
-        col.alias = col.name.replace(tblAliasSing, '');
+        tableCB(table);
+        table.columns.forEach((col) => columnCB(col, table));
 
-        if (col.alias !== 'ID')
-          col.alias = col.alias.replace(/^[A-Z]/, (c) => c.toLowerCase());
+        database.tables.push(table);
       });
 
-      database.tables.push(table);
-    });
-    //console.log(util.inspect(res, {depth: null}));
-    console.log(util.inspect(database, {depth: null}));
-  })
-  .catch(function(err)
-  {
-    console.log('Error');
-    console.log(err);
-  })
-  .finally(function()
-  {
-    infoSchemaDC.getQueryExecuter().getConnectionPool().end();
-  });
+      console.log(util.inspect(database, {depth: null}));
+    })
+    .catch(function(err)
+    {
+      console.log('Error');
+      console.log(err);
+    })
+    .finally(() => this._infoSchemaDC.getQueryExecuter().getConnectionPool().end());
+};
+
+module.exports = Generator;
 
---
+++ example.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var ndm          = require('node-data-mapper');
+var Generator    = require('./Generator');
+var infoSchemaDC = require('./infoSchemaDataContext');
+var generator    = new Generator(infoSchemaDC);
+
+/**
+ * The table alias removes any underscores and uppercases the proceeding
+ * character.  Ex: bike_shop_bikes => bikeShopBikes
+ */
+function tableCB(table)
+{
+  table.alias = table.name.replace(/_[a-z]/g, (c) => c.substr(1).toUpperCase());
+}
+
+/**
+ * Set up each column.
+ */
+function columnCB(col, table)
+{
+  // Add a converter based on the type.
+  if (col.dataType === 'bit')
+    col.converter = ndm.bitConverter;
+}
+
+generator.generateSchema('bike_shop', tableCB, columnCB);
+
