794f105d571dd8a1bccced3cfe8cd82cae3298c4
benbotto <benbotto@users.noreply.github.com>
2016-10-26 15:09:41 -0700

    Updated to use events for table/column modification.

--- example/example.js
+++ example/example.js
@@ -4,15 +4,14 @@ const ndm          = require('node-data-mapper');
 const Generator    = require('ndm-schema-generator').Generator;
 const infoSchemaDC = require('./infoSchemaDataContext');
 const util         = require('util');
-
-let generator = new Generator(infoSchemaDC);
+const generator    = new Generator(infoSchemaDC);
 
 /**
  * The table alias removes any underscores and uppercases the proceeding
  * character.  Ex: bike_shop_bikes => bikeShopBikes
  * @param table A Table object with name and alias properties.
  */
-function tableCB(table) {
+function onAddTable(table) {
   table.alias = table.name.replace(/_[a-z]/g, (c) => c.substr(1).toUpperCase());
 }
 
@@ -22,14 +21,17 @@ function tableCB(table) {
  *        isNullable, maxLength, and isPrimary properties.
  * @param table A Table object with name and alias properties.
  */
-function columnCB(col, table) {
+function onAddColumn(col, table) {
   // Add a converter based on the type.
   if (col.dataType === 'bit')
     col.converter = ndm.bitConverter;
 }
 
+generator.on('ADD_TABLE',  onAddTable);
+generator.on('ADD_COLUMN', onAddColumn);
+
 generator
-  .generateSchema('bike_shop', tableCB, columnCB)
-  .then((schema) => console.log(util.inspect(schema, {depth: null})))
+  .generateSchema('bike_shop')
+  .then(schema => console.log(util.inspect(schema, {depth: null})))
   .catch(console.error);
 
