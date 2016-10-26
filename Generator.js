85ac4d370040dbb1363eb724f44a97e209e88260
benbotto <benbotto@users.noreply.github.com>
2016-10-26 15:07:43 -0700

    Now uses events instead of callbacks when tables and columns are added.

--- Generator.js
+++ Generator.js
@@ -1,43 +1,41 @@
 'use strict';
 
-class Generator {
+const events       = require('events');
+const EventEmitter = events.EventEmitter;
+
+/**
+ * Class that is used to generate a schema object for a database.
+ */
+class Generator extends EventEmitter {
   /**
    * Initialize the schema generator.
-   * @param infoSchemaDC A DataContext instance with permission to read
-   *        from the INFORMATION_SCHEMA table.
+   * @param {DataContext} infoSchemaDC A DataContext instance with permission
+   *        to read from the INFORMATION_SCHEMA table.
    */
   constructor(infoSchemaDC) {
+    super();
     this._infoSchemaDC = infoSchemaDC;
   }
 
   /**
    * Generate the schema from the database.
-   * @param dbName The name of the database for which the schema should be
+   * @param {string} dbName The name of the database for which the schema should be
    *        generated.
-   * @param tableCB A callback function(table) that is called with each
-   *        table.  The function can be used to set the table alias or perform
-   *        other table-level manipulation.
-   * @param columnCB A callback function(column, table) that is called with
-   *        each column.  The function can be used to set a column alias or
-   *        add converters.
    */
-  generateSchema(dbName, tableCB, columnCB) {
-    tableCB  = tableCB  || function() {};
-    columnCB = columnCB || function() {};
-
+  generateSchema(dbName) {
     // Get all the tables and columns from the information_schema db.
-    let query = this._infoSchemaDC
+    const query = this._infoSchemaDC
       .from('tables')
       .innerJoin({
-          table:  'columns',
-          parent: 'tables',
-          on: {
-            $and: [
-              {$eq: {'tables.TABLE_NAME':'columns.TABLE_NAME'}},
-              {$eq: {'tables.TABLE_SCHEMA':'columns.TABLE_SCHEMA'}}
-            ]
-          }
-        })
+        table:  'columns',
+        parent: 'tables',
+        on: {
+          $and: [
+            {$eq: {'tables.TABLE_NAME':'columns.TABLE_NAME'}},
+            {$eq: {'tables.TABLE_SCHEMA':'columns.TABLE_SCHEMA'}}
+          ]
+        }
+      })
       .where({$eq: {'tables.TABLE_SCHEMA':':schema'}}, {schema: dbName})
       .select('tables.TABLE_NAME', 'columns.COLUMN_NAME',
         'columns.DATA_TYPE', 'columns.COLUMN_TYPE',
@@ -47,19 +45,18 @@ class Generator {
 
     return query
       .execute()
-      .then(function(res) {
-        // Fire the table and column callbacks.
-        res.tables.forEach(function(table) {
-          tableCB(table);
-          table.columns.forEach((col) => columnCB(col, table));
+      .then(res => {
+        // Emit events for each table and column so that the user has a chance
+        // to modify them (add converters, alias, etc.).
+        res.tables.forEach(table => {
+          this.emit('ADD_TABLE', table);
+          table.columns.forEach(col => this.emit('ADD_COLUMN', col, table));
         });
 
-        let database = {
+        return {
           name:   dbName,
           tables: res.tables
         };
-
-        return database;
       })
       .finally(() => this._infoSchemaDC.getQueryExecuter().getConnectionPool().end());
   }
